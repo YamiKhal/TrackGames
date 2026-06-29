68cd90ea0e10c925afe05d1fbc4fb436211f33be
Vierdant <vierdant@gmail.com>
2026-06-29 12:45:43 +0200

    undefined value

--- lib/util/rating.ts
+++ lib/util/rating.ts
@@ -1,9 +1,9 @@
 export function ratingToFive(value?: number | null) {
-	if (value === null) return undefined;
+	if (!value) return undefined;
 	return Math.min(5, Math.max(0, value / 20));
 }
 
 export function ratingToHundred(value?: number | null) {
-	if (value === null) return undefined;
+	if (!value) return undefined;
 	return Math.min(100, Math.max(0, value * 20));
 }
